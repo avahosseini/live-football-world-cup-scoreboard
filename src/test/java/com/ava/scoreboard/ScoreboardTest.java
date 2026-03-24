3c239bff286a061091beddb7e1fcd56ee1803210
Ava Hosseini <ava.hssni@gmail.com>
2026-03-24 17:03:01 +0100

    test(scoreboard): add failing ordering test for summary

--- src/test/java/com/ava/scoreboard/ScoreboardTest.java
+++ src/test/java/com/ava/scoreboard/ScoreboardTest.java
@@ -41,7 +41,36 @@ class ScoreboardTest {
                 List.of("Mexico 2 - Canada 1"),
                 scoreboard.getSummary()
         );
-
-
     }
+
+        @Test
+        void shouldReturnSummaryOrderByTotalScoreThenRecency() {
+            InMemoryScoreboard scoreboard = new InMemoryScoreboard();
+
+            scoreboard.startMatch("Mexico", "Canada");
+            scoreboard.updateScore(0, 5);
+
+            scoreboard.startMatch("Spain", "Brazil");
+            scoreboard.updateScore(10, 2);
+
+            scoreboard.startMatch("Germany", "France");
+            scoreboard.updateScore(2, 2);
+
+            scoreboard.startMatch("Uruguay", "Italy");
+            scoreboard.updateScore(6, 6);
+
+            scoreboard.startMatch("Argentina", "Australia");
+            scoreboard.updateScore(3, 1);
+
+            assertEquals(
+                    List.of(
+                            "Uruguay 6 Italy 6",
+                            "Spain 10 - Brazil 2",
+                            "Mexico 0 - Canada 5",
+                            "Argentina 3 - Australia 1",
+                            "Germany 2 - France 2"
+                    ),
+                    scoreboard.getSummary()
+            );
+        }
 }
